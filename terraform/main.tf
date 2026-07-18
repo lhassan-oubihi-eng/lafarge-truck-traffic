59916bdd912f0e00d33ba12c322a705e56bd97bd
lhassan-oubihi <lhassan.oubihi1@gmail.com>
2026-07-18 15:26:21 +0200

    feat: enable ASG auto-refresh on launch template changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -399,6 +399,7 @@ resource "aws_autoscaling_group" "app" {
 
   instance_refresh {
     strategy = "Rolling"
+    triggers = ["launch_template"]
     preferences {
       min_healthy_percentage = 50
       instance_warmup        = 60
